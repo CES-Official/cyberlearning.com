d8d528e42471e02b1b3cd1fb61833e32775392a4
Udit <disposabledocs@gmail.com>
2026-01-23 20:49:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.get("/api/certifications", async (req, res) => {
    🚀 START SERVER
    ========================================================= */
 
-const PORT = process.env.PORT || 3000;
+const PORT = "3000";
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
