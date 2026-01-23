e0223c7acd2093137fed2ef96292e49c72007329
Udit <disposabledocs@gmail.com>
2026-01-23 20:39:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -118,6 +118,11 @@ const apiLimiter = rateLimit({
 
 app.use("/api", apiLimiter);
 
+app.use((req, res, next) => {
+  console.log("➡️", req.method, req.url);
+  next();
+});
+
 /* =========================================================
    🔓 PUBLIC HTML PAGES
    ========================================================= */
